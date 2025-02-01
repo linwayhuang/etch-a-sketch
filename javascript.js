4ea25bb6227e42d96ba33ebfed3717e16f6f2e1b
Jackson Bond <111647134+linwayhuang@users.noreply.github.com>
2025-02-01 13:21:18 -0600

    Create layout of the game

---
+++ javascript.js
@@ -0,0 +1,10 @@
+//Set variables
+const container = document.querySelector("#container");
+const sideBar = document.querySelector("#sideBar");
+const canvas = document.querySelector("#canvas");
+
+//Create elements, attach to other elements, and set variables
+const pixel = document.createElement("div");
+pixel.id = "pixel";
+canvas.appendChild(pixel);
+canvas.appendChild(pixel);
