61787b8e5c36451952226d3a26ad5916515d0e73
Jackson Bond <111647134+linwayhuang@users.noreply.github.com>
2025-02-07 00:46:59 -0600

    Add working buttons for Rainbow and Shade mode, add slider for grid size

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 //Set variables
 const container = document.querySelector("#container");
 const sideBar = document.querySelector("#sideBar");
+const slider = document.querySelector("#slider");
+const gridSize = document.querySelector("#gridSize");
 const canvas = document.querySelector("#canvas");
 
 //Create elements, attach to other elements, and set variables
@@ -11,56 +13,87 @@ buttonBrush.id = "buttonBrush";
 buttonBrush.textContent = "Brush Mode";
 sideBar.appendChild(buttonBrush);
 
-//Initial event listeners
+let buttonRainbow = document.createElement("button");
+buttonRainbow.id = "buttonRainbow";
+buttonRainbow.textContent = "Rainbow Mode";
+sideBar.appendChild(buttonRainbow);
+
+let buttonShade = document.createElement("button");
+buttonShade.id = "buttonShade";
+buttonShade.textContent = ("Shade Mode");
+sideBar.appendChild(buttonShade);
+
+gridSize.textContent = `${slider.value} x ${slider.value}`;
+
+//You can use the below method to select the elements based on class. This class can even be created inside a loop.
+const pixels = document.querySelectorAll(".square");
+
+//Start
+slider.addEventListener("input", (event) => {
+    gridSize.textContent = event.target.value;
+});
+
+//For some reasons, when you have two variables (one for row and one for column) in the canvasSize function, "canvasSize()" doesn't work. I had to put in an argument to make it work
+//If you have one variable in the canvasSize function, "canvasSize()" work
+canvasSize(slider.value);
+
 sideBar.addEventListener("click", (event) => {
     let target = event.target;
 
     switch(target.id) {
         case "buttonBrush":
-            alert("you win");
+            alert("Brush Mode");
+            pixels.forEach(pixel => {
+                pixel.addEventListener("mouseenter", () => {
+                    pixel.style.backgroundColor = "rgb(155, 102, 102)"; //if you put the rgb values in back bracket then it doesn't work
+                });
+            });
+            break;
+        case "buttonRainbow":
+            alert("Rainbow Mode");
+            pixels.forEach(pixel => {
+                pixel.addEventListener("mouseenter", () => {
+                    pixel.style.backgroundColor = randomColor();
+                });
+            });
+            break;
+        case "buttonShade":
+            alert("Shade Mode");
+            pixels.forEach(pixel => {
+                let i = 0.1;
+                if (i < 1) {
+                    pixel.addEventListener("mouseenter", () => {
+                        pixel.style.backgroundColor = "black";
+                        pixel.style.opacity = `${i}`;
+                        i += 0.1;
+                    });
+                }
+            });
             break;
     }
 });
 
-//Start
-
-//You can create an array so you can give the square unique ID, another way is that you can create a unique ID directly using ${} and class/id
-const squares = [];
-//For some reasons, when you have two variables (one for row and one for column) in the canvasSize function, "canvasSize()" doesn't work. I had to put in an argument to make it work
-//If you have one variable in the canvasSize function, "canvasSize()" work
-canvasSize(5);
-
-//You can use the below method to select the elements based on class. This class can even be created inside a loop.
-const pixels = document.querySelectorAll(".square");
-pixels.forEach(pixel => {
-    pixel.addEventListener("mouseenter", () => {
-        pixel.style.backgroundColor = "black";
-    });
-});
-
-pixels.forEach(pixel => {
-    pixel.addEventListener("mouseleave", () => {
-        pixel.style.backgroundColor = "";
-    });
-});
-
 //Functions
-//You can't create divs by coping and pasting the same code block, but you can create multiples of them
-//by using the loop. I don't know why and how the programs treats the "pixel" variable but we'll see
+//You can't create divs by copying and pasting the same code block, but you can create multiples of them
+//by using the loop. You can access the divs from outside of the loop by its class/id
 function canvasSize (size = 16) {
     for (i = 0; i < (size * size); i++) {
         let square = document.createElement("div");
         square.classList.add("square");
         canvas.appendChild(square);
-        square.setAttribute("style", `border-radius: ${60 / size}px; width: ${600 / size}px; height: ${600 / size}px;`);
-        squares.push(square);
+        square.setAttribute("style", `border-radius: ${70 / size}px; width: ${700 / size}px; height: ${700 / size}px;`);
     }
 }
 
-function hover () {
-
+function randomColor () {
+    let r = Math.floor(Math.random() * 257); //return a random integer between 0 and 256
+    let g = Math.floor(Math.random() * 257); //return a random integer between 0 and 256
+    let b = Math.floor(Math.random() * 257); //return a random integer between 0 and 256
+    return `rgb(${r}, ${g}, ${b})`;
 }
 
 //add borders: https://bscottnz.github.io/esketch/
 //https://aroan-v.github.io/Etch-A-Sketch/
 //add addjustable grid length based on adjusted canvas size
+//make the grid change color only when the left button is pressed. Also make the hover effect on
+//each grid whereever the mouse enter but it won't do anything until the click
